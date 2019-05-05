ad4030b8bc0a7d5479656f94369095ad279ba1d8
eggmund <joshuacolclough2@googlemail.com>
2019-05-05 22:09:36 +0100

    Icosohedron drawing.

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,53 @@ use std::rc::Rc;
 
 const TAO: f32 = 1.618033988749895;
 
-fn generate_icosohedron(radius: f32) -> Vec<Point3<f32>> {
+fn generate_icosohedron(radius: f32) -> (Vec<Point3<f32>>, Vec<Point3<u16>>) {
    let mut ico: Vec<Point3<f32>> =
-   vec![Point3::new(-1.0, TAO, 0.0),
-        Point3::new(1.0, TAO, 0.0),
-        Point3::new(-1.0, 0.447, 0.851),
-        Point3::new(1.0, 0.447, 0.526),
+      vec![Point3::new(-1.0, TAO, 0.0),   // 0
+           Point3::new(1.0, TAO, 0.0),    // 1
+           Point3::new(-1.0, -TAO, 0.0),  // 2
+           Point3::new(1.0, -TAO, 0.0),   // 3
 
-        Point3::new(0.0, -1.0, TAO),
-        Point3::new(0.0, 1.0, TAO),
-        Point3::new(0.0, -1.0, -TAO),
-        Point3::new(0.0, -1.0, -TAO),
+           Point3::new(0.0, -1.0, TAO),   // 4
+           Point3::new(0.0, 1.0, TAO),    // 5
+           Point3::new(0.0, -1.0, -TAO),  // 6
+           Point3::new(0.0, 1.0, -TAO),   // 7
 
-        Point3::new(TAO, 0.0, -1.0),
-        Point3::new(TAO, 0.0, 1.0),
-        Point3::new(-TAO, 0.0, -1.0),
-        Point3::new(-TAO, 0.0, 1.0)];
+           Point3::new(TAO, 0.0, -1.0),   // 8
+           Point3::new(TAO, 0.0, 1.0),    // 9
+           Point3::new(-TAO, 0.0, -1.0),  //10
+           Point3::new(-TAO, 0.0, 1.0)];  //11
+
+   let faces: Vec<Point3<u16>> =
+      vec![Point3::new(0, 11, 5),
+           Point3::new(0, 5, 1),
+           Point3::new(0, 1, 7),
+           Point3::new(0, 7, 10),
+           Point3::new(0, 10, 11),
+
+           Point3::new(1, 5, 9),
+           Point3::new(5, 11, 4),
+           Point3::new(11, 10, 2),
+           Point3::new(10, 7, 6),
+           Point3::new(7, 1, 8),
+
+           Point3::new(3, 9, 4),
+           Point3::new(3, 4, 2),
+           Point3::new(3, 2, 6),
+           Point3::new(3, 6, 8),
+           Point3::new(3, 8, 9),
+
+           Point3::new(4, 9, 5),
+           Point3::new(2, 4, 11),
+           Point3::new(6, 2, 10),
+           Point3::new(8, 6, 7),
+           Point3::new(9, 8, 1)];
 
    for i in 0..ico.len() {
      ico[i] *= radius
    }
 
-   ico
+   (ico, faces)
 }
 
 fn main() {
@@ -41,14 +66,10 @@ fn main() {
    let b = Point3::new(1.0, -1.0, 0.0);
    let c = Point3::new(0.0, 1.0, 0.0);
 
-   let vertices = generate_icosohedron(1.0);   //vec![a, b, c];
-   let mut indices = vec![];
-   for i in 0u16..10 {
-      indices.push(Point3::new(i, i + 1, i + 2));
-   }
+   let (vertices, faces) = generate_icosohedron(1.0);   //vec![a, b, c];
 
    let mesh = Rc::new(RefCell::new(Mesh::new(
-      vertices, indices, None, None, false,
+      vertices, faces, None, None, false,
    )));
    let mut c = window.add_mesh(mesh, Vector3::new(1.0, 1.0, 1.0));
 
