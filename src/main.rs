04872b54bb0bb6c37ccb2c1e7c7d2852e1c2baa5
eggmund <joshuacolclough2@googlemail.com>
2019-05-06 17:19:05 +0100

    Sphere is being made!

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,18 @@ use std::rc::Rc;
 fn main() {
    let mut window = Window::new("Kiss3d: custom_mesh");
 
-   let pl = Planet::generate(1.0);   //vec![a, b, c];
-
+   let pl = Planet::generate(2.0, 2);
    let mut c = window.add_mesh(Rc::new(RefCell::new(pl.mesh)), Vector3::new(1.0, 1.0, 1.0));  // Add planet's mesh to window
-
    c.set_color(0.9, 0.9, 0.9);
    c.enable_backface_culling(true);
    c.set_local_translation(Translation::from(Vector3::new(0.0, 0.0, 10.0)));
+
+   let pl2 = Planet::generate(2.0, 0);
+   let mut c2 = window.add_mesh(Rc::new(RefCell::new(pl2.mesh)), Vector3::new(1.0, 1.0, 1.0));  // Add planet's mesh to window
+   c2.set_color(0.9, 0.9, 0.9);
+   c2.enable_backface_culling(true);
+   c2.set_local_translation(Translation::from(Vector3::new(5.0, 0.0, 10.0)));
+
 
    window.set_light(Light::StickToCamera);
 
